14ec2357e35b76e57c3f00ab48e553828fff96e5
Gabriel Marchetti dee Souza <gbielmarch@gmail.com>
2023-04-28 10:29:16 -0300

    Debito a ser testado

--- pagamento/metodopagamento/modalidadepagamento/Debito.cs
+++ pagamento/metodopagamento/modalidadepagamento/Debito.cs
@@ -7,6 +7,19 @@ namespace Modalidade_Pagamento
 {
     public class Debito
     {
-        
+    //         - Débito : valor á vista sem desconto
+    //      - valor do saldo em conta deve ser pré-definido
+        private float saldo;
+
+        public float Depositar( float deposito)
+        {
+            if( deposito > 0)
+            {
+                saldo = deposito;
+                return saldo;
+            }
+            return 0;
+        }
+        // Saldo do Debito determinado, deixa que o clienta determine o saldo 
     }
 }
