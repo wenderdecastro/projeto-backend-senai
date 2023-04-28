8b4f0fb320add746af6d3175a60a23f8f57257fe
Wender <wendermaiac@gmail.com>
2023-04-28 08:54:34 -0300

    classes instanciadas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Projeto_Backend_Senai;
+
+Pagamento payment = new Pagamento();
+Cartao card = new Cartao();
+Credito credit = new Credito();
+Debito debit = new Debito();
+Boleto bankSlip = new Boleto();
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
