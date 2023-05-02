3262b5c7f950dccca226e65d0818f2b61eba3a1e
guilherme amorim <cezarguilherme03@gmail.com>
2023-05-02 10:50:48 -0300

    Finalizando algumas alterações visuais

--- pagamento/metodopagamento/Boleto.cs
+++ pagamento/metodopagamento/Boleto.cs
@@ -6,7 +6,7 @@ using Projeto_Backend_Senai;
 
 namespace Metodo_Pagamento
 {
-    public class Boleto : Projeto_Backend_Senai.Pagamento 
+    public class Boleto : Projeto_Backend_Senai.Pagamento
     {
         Ferramentas escreva = new Ferramentas();
         public bool escolhaPagamento;
@@ -19,33 +19,33 @@ namespace Metodo_Pagamento
 
             ValorPagamentoBoleto = valorInput * 0.88;
 
-            Console.WriteLine($"O valor do pagamento em boleto tem um desconto de 12% do valor total, portanto, o valor com o desconto ficou de: {Math.Round(ValorPagamentoBoleto, 2).ToString("C")} reais");
+            escreva.Escrever($"O valor do pagamento em boleto tem um <+Yellow> desconto de 12% do valor total </>, portanto, o valor com o desconto ficou de: <+Green>{Math.Round(ValorPagamentoBoleto, 2).ToString("C")} reais </>");
 
-            Console.WriteLine($"O valor do código de barras de sua fatura é: {CodigoDeBarras.Next(1000)}90.57670 {CodigoDeBarras.Next(100000)}.{CodigoDeBarras.Next(100000)} 7 {CodigoDeBarras.Next(100000)}.{CodigoDeBarras.Next(100000)}");
+            escreva.Escrever($"O valor do código de barras de sua fatura é: <=Red>{CodigoDeBarras.Next(1000)}90.57670 {CodigoDeBarras.Next(100000)}.{CodigoDeBarras.Next(100000)} 7 {CodigoDeBarras.Next(100000)}.{CodigoDeBarras.Next(100000)} </>");
 
             do
             {
-                Console.WriteLine(@$" 
+                escreva.Escrever(@" 
             Selecione:
-            [1] - Para realizar o pagamento da fatura via boleto;
-            [2] - Para voltar para o menu de escolha de pagamento; 
+           <+Blue> [1] </> - Para realizar o pagamento da fatura via boleto;
+           <+Blue> [2] </> - Para voltar para o menu de escolha de pagamento; 
             ");
                 ConsoleKeyInfo escolhaMenu = Console.ReadKey(true);
 
                 //colocando readKey
-                switch (escolhaMenu.Key) 
+                switch (escolhaMenu.Key)
                 {
                     case ConsoleKey.D1:
-//   Escrever("<+Green> iae </> quqoweruqwef ");
-                        // Console.ForegroundColor = ConsoleColor.Green;
+                       
                         escreva.Escrever($"<+Green> Pagamento efetuado com sucesso! </> Obrigado por utilizar nosso programa!");
                         escolhaPagamento = true;
+
                         break;
                     case ConsoleKey.D2:
 
-                        // Console.ForegroundColor = ConsoleColor.Red;
                         escreva.Escrever($"<+Red> Voce cancelou a operação de pagamento em Boleto </>, até mais!");
                         escolhaPagamento = true;
+
                         break;
                     default:
 
